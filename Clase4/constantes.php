d266763f5ddbdea76e3d6979227c1859e0138764
SirJulianM <ing_julianmadrid@hotmail.com>
2023-07-20 19:51:01 -0500

    Manejo de constantes en PHP

---
+++ Clase4/constantes.php
@@ -0,0 +1,10 @@
+<?php  
+
+/*
+* Constantes: Son espacio en memoria que almacenan información. Se crean con la función define() que recibe dos datos 
+* separados por coma: el nombre de la constante y el valor de esta
+*/
+
+define('NOMBRE_SITIO', "./index.php");
+
+?>
